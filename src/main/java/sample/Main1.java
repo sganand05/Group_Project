22ffea42076c140cbaa77cee8d0b517e08d94bf2
Sanjai451 <sanjaikumarr451@gmail.com>
2026-03-28 08:37:44 +0530

    updates from sanjai

--- src/main/java/sample/Main1.java
+++ src/main/java/sample/Main1.java
@@ -3,5 +3,6 @@ package sample;
 public class Main1 {
 	public static void main(String[] args) {
 		GFIle.msg();
+		SFIle.msg();
 	}		
 }
---
+++ src/main/java/sample/SFIle.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class SFIle {
+	public static void msg() {
+		System.out.println("Message from Sanjai Kumar R");
+	}
+}
