9ef8947a63edd3299967db860aa3a257dc0e4b62
Gokul Anand S <sgokulanand0505@gmail.com>
2026-03-28 08:27:15 +0530

    from gokul 1nd update

--- src/main/java/sample/GFIle.java
+++ src/main/java/sample/GFIle.java
@@ -2,6 +2,9 @@ package sample;
 
 public class GFIle {
 	public static void msg() {
-		System.out.println("Hello from gokul");
+		System.out.println("Hello from Gokul Anand");
+	}
+	public static void msg2() {
+		System.out.println("Hello from Gokul Anand (2)");
 	}
 }
